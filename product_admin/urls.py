73ea8d2ad661f1a56f756cc11e32bd99e3cd7f2c
wangleiz166 <wangleiz166@126.com>
2023-05-05 18:32:41 +0100

    add function

--- product_admin/urls.py
+++ product_admin/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.list, name='list'),
     path('userlist/', views.userList, name='userList'),
-    path('dashboard/', views.dashboard, name='dashboard'),    
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('set_admin/<int:user_id>/', views.set_admin, name='set_admin'),   
+    path('set_general/<int:user_id>/', views.set_general, name='set_general'),  
 ]
